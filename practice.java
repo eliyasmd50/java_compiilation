60c941acc1cc0995baa99a05a17acc25b6bbe8c6
eliyasmd50 <eliyasmohamed50@gmail.com>
2024-09-06 08:36:18 +0530

    Fundamentals Practice

---
+++ practice.java
@@ -0,0 +1,58 @@
+class practice {
+    public static void main (String [] args) {
+        //Strings in java
+        String name;
+        name= "Eliyas";
+        name="Mohamed Eliyas";
+        String fatherName = "sulthan";
+        System.out.println("Name: "+name);
+        System.out.println("Fathers Name: "+fatherName);//concatination of string
+
+        //integer in Java 
+        int age;
+        age = 25;
+        System.out.println("Age: "+age);
+
+        //double in java for more precise use like daling with bank balance
+        double average = 79.89;
+        System.out.println("Avrage Marks: "+average);
+
+        //float in java
+        float overallAverage = 80.95f;// same as double but it is a but lighter compared to double and it is preferred
+        System.out.println("Overall Average: "+overallAverage);
+
+
+        //boolean in java
+        boolean isEligibleBachelor = true;
+        System.out.println("Bachelor: "+isEligibleBachelor);
+
+        //sum of two number
+        int a=5;
+        int b=6;
+        System.out.println(a+b);
+
+        //subtraction of two number
+        int x=8;
+        int y=6;
+        int z=x-y;
+        System.out.println(z);
+
+        //multiplication of two number
+        int m=8;
+        int n=6;
+        int o=m*n;
+        System.out.println(o);
+
+        //division of two number gives the quotient as output
+        int value1=8;
+        int value2=6;
+        float value3=value1/value2;
+        System.out.println(value3);
+
+         //module of two number gives remaindeer as output
+         int q=8;
+         int r=6;
+         int s=q%r;
+         System.out.println(s);
+    }
+}
