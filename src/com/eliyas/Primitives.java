0fe70893449cc396e216b46cafe27036d6a6348c
eliyasmd50 <eliyasmohamed50@gmail.com>
2024-12-09 19:33:46 +0530

    changes done for primitive datatypes

---
+++ src/com/eliyas/Primitives.java
@@ -0,0 +1,15 @@
+package com.eliyas;
+
+public class Primitives {
+    public static void main(String[] args) {
+        // primitive data type which cannot be separate into another data type
+        int number = 65;
+        char letter = 'r';
+        float marks = 65.78f;
+        long largeInteger = 242345435436456435L;
+        double largeFloat = 545435.434324;
+        boolean isChecked = true;
+
+        Integer rno = 56;
+    }
+}
