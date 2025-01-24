d02e278d54efabd5a8035d50b649821f4d521f48
Mohamed Eliyas <mohamed.eliyas@photon.com>
2025-01-24 20:23:48 +0530

    initial push for java

---
+++ src/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("HEllow world");
+        System.out.println("HEllow world");
+        String Variable = "ELiyas";
+        System.out.println(Variable);
+    }
+}
